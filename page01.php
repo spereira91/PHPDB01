4f335fe23f7495738a74e574382a60feb9b952cf
spereira91 <sandrotqr@gmail.com>
2022-04-06 16:40:44 -0300

    AULA2

---
+++ page01.php
@@ -0,0 +1,19 @@
+<?php
+// cria uma variável
+$carro = "Fusca"
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php echo $carro ?>
+
+</body>
+</html>
